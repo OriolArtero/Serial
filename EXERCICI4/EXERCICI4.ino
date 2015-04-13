132b66e5335921347dc8d0f11616312734356f10
OriolArtero <Jackesg_92@hotmail.com>
2015-04-13 20:57:22 +0200

    EXERCICI 4

---
+++ EXERCICI4/EXERCICI4.ino
@@ -0,0 +1,110 @@
+         
+
+void  setup ()
+{
+  pinMode (5, OUTPUT);      
+  pinMode (6, OUTPUT);      
+  pinMode (7, OUTPUT);      
+  pinMode (8, OUTPUT);      
+      
+}
+
+
+void  loop ()
+{
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, HIGH);
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(1000);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, HIGH);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+  
+  digitalWrite (5, LOW);    
+  digitalWrite (6, LOW);    
+  digitalWrite (7, LOW);   
+  digitalWrite (8, LOW);     
+  delay(100);
+}  
